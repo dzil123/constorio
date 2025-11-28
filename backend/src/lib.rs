1f3f6edf21f0cc275545ef8f01ba983846a950ca
dzil123 <5725958+dzil123@users.noreply.github.com>
2025-11-28 02:39:49 -0800

    a

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,16 +1,44 @@
-use std::{
-    marker::PhantomData,
-    ops::{Add, Div},
-};
+use std::{marker::PhantomData, ops::*};
 
-pub use typenum::{self, NonZero, Unsigned, consts::*};
+pub use typenum::{self, NonZero, Unsigned, consts::*, operator_aliases::*, type_operators::*};
 
 use crate::new::New;
 
 mod new;
 
-pub trait Resource: private::Sealed {
+macro_rules! make_fn {
+    {
+        $(<$T:ty, $U:ty>),* $(,)?
+        $(Quot<$Tdiv:ty, $Udiv:ty>),*
+        ; $($stub:tt)*
+    } => {
+        $($stub)*
+        where
+        $(
+            $T: Unsigned + Add<$U> + Max<$U> + Mul<$U>,
+            $U: Unsigned,
+            Sum<$T, $U>: Unsigned,
+            Maximum<$T, $U>: Unsigned,
+            Prod<$T, $U>: Unsigned,
+        )*
+        $(
+            $Tdiv: Unsigned + Div<$Udiv>,
+            $Udiv: Unsigned,
+            Quot<$Tdiv, $Udiv>: Unsigned,
+        )*
+        { New::NEW }
+    }
+}
+
+pub trait Resource: private::Sealed {}
+pub trait CanMine: Resource {
     type MiningTicks: Unsigned + NonZero; // time to mine one of resource
+}
+pub trait CanSmelt: Resource {
+    type SmeltTicks: Unsigned + NonZero;
+    type SmeltInCount: Unsigned + NonZero;
+    type SmeltOutCount: Unsigned + NonZero;
+    type SmeltOutput: Resource;
 }
 
 // TS means Timestamp
@@ -18,78 +46,90 @@ pub struct Bundle<R: Resource, N: Unsigned, TS: Unsigned>(PhantomData<(R, N, TS)
 impl_new!(Bundle, R, Resource, N, Unsigned, TS, Unsigned);
 
 impl<R: Resource, N: Unsigned, TS: Unsigned> Bundle<R, N, TS> {
-    pub const fn ffwd<Duration: Addable<TS>>(self) -> Bundle<R, N, Duration::Sum>
-    {
-        New::NEW
+    make_fn! {
+        <TS, Duration>;
+        pub const fn ffwd<Duration>(self) -> Bundle<R, N, Sum<TS, Duration>>
     }
 
-    /// combine two bundles with same resource and timestamp but different N counts into the sum
-    pub const fn combine<N2: Addable<N>>(self, _: Bundle<R, N2, TS>) -> Bundle<R, N2::Sum, TS>
-    {
-        New::NEW
+    make_fn! {
+        <N, N2>, <TS, TS2>;
+        pub const fn combine<N2, TS2>(self, _: Bundle<R, N2, TS2>) -> Bundle<R, Sum<N, N2>, Maximum<TS, TS2>>
     }
 }
 
-pub struct Iron(PhantomData<()>);
-pub type IronBundle<N, TS> = Bundle<Iron, N, TS>;
-
-impl private::Sealed for Iron {}
-impl Resource for Iron {
+pub struct IronOre(PhantomData<()>);
+impl private::Sealed for IronOre {}
+impl Resource for IronOre {}
+impl CanMine for IronOre {
     type MiningTicks = U2;
 }
+impl CanSmelt for IronOre {
+    type SmeltTicks = U3;
+    type SmeltInCount = U1;
+    type SmeltOutCount = U2;
+    type SmeltOutput = IronIngot;
+}
 
-pub struct Miner<R: Resource, TS: Unsigned>(PhantomData<(R, TS)>);
-impl_new!(Miner, R, Resource, TS, Unsigned);
+pub struct IronIngot(PhantomData<()>);
+impl private::Sealed for IronIngot {}
+impl Resource for IronIngot {}
+
+pub struct Miner<R: CanMine, TS: Unsigned>(PhantomData<(R, TS)>);
+impl_new!(Miner, R, CanMine, TS, Unsigned);
 pub type NewMiner<R> = Miner<R, U0>;
 
-pub trait Addable<Rhs: Unsigned>: Unsigned {
-    type Sum: Unsigned;
-}
-impl<T: Unsigned, U: Unsigned, V: Unsigned> Addable<U> for T
-where
-    T: Add<U, Output = V>,
-{
-    type Sum = V;
-}
-
-pub trait Divable<Rhs: Unsigned>: Unsigned {
-    type Quotient: Unsigned;
-}
-impl<T: Unsigned, U: Unsigned, V: Unsigned> Divable<U> for T
-where
-    T: Div<U, Output = V>,
-{
-    type Quotient = V;
-}
-
-impl<R: Resource, TS: Unsigned> Miner<R, TS> {
-    pub const fn ffwd<Duration: Addable<TS>>(self) -> Miner<R, Duration::Sum>
-    {
-        New::NEW
+impl<R: CanMine, TS: Unsigned> Miner<R, TS> {
+    make_fn! {
+        <TS, Duration>;
+        pub const fn ffwd<Duration>(self) -> Miner<R, Sum<TS, Duration>>
     }
 
-    pub const fn mine_for_duration<Duration: Addable<TS> + Divable<R::MiningTicks>>(
-        self
-    ) -> (
-        Miner<R, Duration::Sum>,
-        Bundle<R, Duration::Quotient, Duration::Sum>,
-    )
-    {
-        New::NEW
+    make_fn! {
+        <TS, Duration>,
+        Quot<Duration, R::MiningTicks>;
+        pub const fn mine_for_duration<Duration>(self) -> (
+            Miner<R, Sum<TS, Duration>>,
+            Bundle<R, Quot<Duration, R::MiningTicks>, Sum<TS, Duration>>,
+        )
     }
 }
 
-type GameFunction<TS> = fn(NewMiner<Iron>) -> (Bundle<Iron, U5, TS>,);
+pub struct Furnace<R: CanSmelt, TS: Unsigned>(PhantomData<(R, TS)>);
+impl_new!(Furnace, R, CanSmelt, TS, Unsigned);
+impl<R: CanSmelt, TS: Unsigned> Furnace<R, TS> {
+    make_fn! {
+        <TS, TS2>, <NIn, R::SmeltTicks>,
+        <Maximum<TS, TS2>, Prod<NIn, R::SmeltTicks>>,
+        <Quot<NIn, R::SmeltInCount>, R::SmeltOutCount>
+        Quot<NIn, R::SmeltInCount>;
+        pub const fn smelt_all<NIn, TS2>(self, _: Bundle<R, NIn, TS2>) -> (
+            Furnace<
+                R,
+                Sum<Maximum<TS, TS2>, Prod<NIn, R::SmeltTicks>>
+            >,
+            Bundle<
+                R::SmeltOutput,
+                Prod<Quot<NIn, R::SmeltInCount>, R::SmeltOutCount>,
+                Sum<Maximum<TS, TS2>, Prod<NIn, R::SmeltTicks>>
+            >,
+        )
+    }
+}
+
+type GameFunction<TS> = fn(NewMiner<IronOre>) -> (Bundle<IronOre, U5, TS>,);
 pub fn run<TS: Unsigned>(func: GameFunction<TS>) -> usize {
-    let _: (Bundle<Iron, U5, TS>,) = (func)(New::NEW);
+    let _: (Bundle<IronOre, U5, TS>,) = (func)(New::NEW);
     println!("successfully completed game in {} ticks", TS::USIZE);
     TS::USIZE
 }
 
-type GameFunction2<TS> = fn(NewMiner<Iron>, NewMiner<Iron>) -> (Bundle<Iron, U5, TS>,);
+type GameFunction2<TS> = fn(NewMiner<IronOre>, NewMiner<IronOre>) -> (Bundle<IronOre, U5, TS>,);
 pub fn run2<TS: Unsigned>(func: GameFunction2<TS>) -> usize {
-    let _: (Bundle<Iron, U5, TS>,) = (func)(New::NEW, New::NEW);
-    println!("successfully completed game with 2 miners in {} ticks", TS::USIZE);
+    let _: (Bundle<IronOre, U5, TS>,) = (func)(New::NEW, New::NEW);
+    println!(
+        "successfully completed game with 2 miners in {} ticks",
+        TS::USIZE
+    );
     TS::USIZE
 }
 
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,5 @@
+// #![recursion_limit = "512"]
+
 use backend::*;
 
 fn main() {
@@ -13,20 +15,23 @@ unsafe extern "C" fn foobar() -> usize {
     run(my_game)
 }
 
-const fn my_game(miner: NewMiner<Iron>) -> (Bundle<Iron, U5, impl Unsigned>,) {
+const fn my_game(miner: NewMiner<IronOre>) -> (Bundle<IronOre, U5, impl Unsigned>,) {
     let (miner, bundle1) = miner.mine_for_duration::<U10>();
     (bundle1,)
 }
 
-const fn my_game2(miner: NewMiner<Iron>) -> (Bundle<Iron, U5, impl Unsigned>,) {
+const fn my_game2(miner: NewMiner<IronOre>) -> (Bundle<IronOre, U5, impl Unsigned>,) {
     let (miner1, bundle1) = miner.mine_for_duration::<U6>();
     let (miner2, bundle2) = miner1.mine_for_duration::<U4>();
     let (miner3, bundle3) = miner2.mine_for_duration::<U0>();
-    (bundle2.ffwd::<U0>().combine(bundle1.ffwd::<U4>()).combine(bundle3),)
+    (bundle2.combine(bundle1).combine(bundle3),)
 }
 
-const fn my_game3(miner1: NewMiner<Iron>, miner2: NewMiner<Iron>) -> (Bundle<Iron, U5, impl Unsigned>,) {
+const fn my_game3(
+    miner1: NewMiner<IronOre>,
+    miner2: NewMiner<IronOre>,
+) -> (Bundle<IronOre, U5, impl Unsigned>,) {
     let (miner1, bundle1) = miner1.mine_for_duration::<U6>();
     let (miner2, bundle2) = miner2.mine_for_duration::<U4>();
-    (bundle2.ffwd::<U2>().combine(bundle1.ffwd::<U0>()),)
+    (bundle2.combine(bundle1),)
 }
